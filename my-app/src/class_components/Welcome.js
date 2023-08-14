769fb1efe976d50827b0a1e94b7792b3aa9ec903
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-14 11:28:23 +0300

    Destructuring (states) inside the render method in class component

--- my-app/src/class_components/Welcome.js
+++ my-app/src/class_components/Welcome.js
@@ -4,6 +4,7 @@ class Welcome extends Component {
   render() {
     console.log(this.props.children) // not working with class component
     const {name, heroName} = this.props
+    // const {state1, state2} = this.state
     return (
     <div>
       <h1>Welcome {name} a.k.a {heroName}</h1>
