32c8895c77c3e08518372c59529815618766aaee
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-14 11:23:20 +0300

    Destructuring inside the function body

--- my-app/src/func_components/Greet.js
+++ my-app/src/func_components/Greet.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const Greet = ({name, heroName, children}) => {
+const Greet = (props) => {
+    console.log(props)
+    const {name, heroName, children} = props
     return (
         <div>
             <h1>Hello {name} a.k.a {heroName}</h1>
