4eba3074b1190f0ab47e19f77d872b8bdeb9a7b8
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-27 23:54:43 +0300

    List Rendering

---
+++ Lists/src/components/NameList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Person from './Person'
+
+function NameList() {
+  const persons = [
+    {
+        id: 1,
+        name: 'Igor',
+        age: '29',
+        skill: 'React'
+    },
+    {
+        id: 2,
+        name: 'Robert',
+        age: 27,
+        skill: 'Python'
+    },
+    {
+        id: 3,
+        name: 'Cynthia',
+        age: 24,
+        skill: 'MongoDB'
+    },
+    {
+        id: 4,
+        name: 'Elsie',
+        age: 21,
+        skill: 'SQL Server'
+    }
+  ]
+  const personList = persons.map(person => (
+    <Person person={person} />
+  ))
+
+  return (
+    <div>{personList}</div>
+  )
+}
+
+export default NameList
