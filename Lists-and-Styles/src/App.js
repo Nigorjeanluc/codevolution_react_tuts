321a1db8f496520e6b8742fe04aa2c6182d2b51f
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-29 09:17:00 +0300

    CSS modules

--- Lists-and-Styles/src/App.js
+++ Lists-and-Styles/src/App.js
@@ -5,11 +5,16 @@ import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
 
+import './appStyles.css'
+import styles from './appStyles.module.css' // this is a module stylesheet (CSS module do not apply to child components)
+
 function App() {
   return (
     <div className="App">
       <Inline />
       <Stylesheet primary={true} />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
       {/* <NameList /> */}
     </div>
   );
