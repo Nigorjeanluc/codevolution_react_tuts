9f18d95d44b88e1226c957c3dbb7a4eaba83f984
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-29 08:59:43 +0300

    External file stylesheet

--- Lists-and-Styles/src/App.js
+++ Lists-and-Styles/src/App.js
@@ -2,11 +2,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <Stylesheet />
+      {/* <NameList /> */}
     </div>
   );
 }
