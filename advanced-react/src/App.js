0eeb65fb436d61b1904985940b8b1fcda641c646
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-30 19:02:36 +0300

    pure components for performance boost

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
 
 function App() {
   return (
     <div className="App">
-      <FragmentDemo />
-      <Table />
+      {/* <FragmentDemo />
+      <Table /> */}
+      <ParentComp />
     </div>
   );
 }
