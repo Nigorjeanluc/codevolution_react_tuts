a56e74b3e1fdf07110f5939992a25d3a1a4e292b
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-31 09:37:57 +0300

    Forwarding refs

--- react-refs/src/App.js
+++ react-refs/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 function App() {
   return (
     <div className="App">
       {/* <RefsDemo /> */}
-      <FocusInput />
+      {/* <FocusInput /> */}
+      <FRParentInput />
     </div>
   );
 }
