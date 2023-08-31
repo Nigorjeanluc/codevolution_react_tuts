cf7995f2e1eccef5da15f81fd496fccbf8a425e9
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-31 20:32:50 +0300

    common functionality need to be shared between components scenario

---
+++ higher-order-components/src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import logo from './logo.svg';
+import './App.css';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <ClickCounter />
+      <HoverCounter />
+    </div>
+  );
+}
+
+export default App;
