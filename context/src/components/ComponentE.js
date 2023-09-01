ea6bae34d34d2895ad62e0b6b7fdf5815f2a744e
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-09-01 19:25:46 +0300

    Use of context to pass data without props in nested components

---
+++ context/src/components/ComponentE.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ComponetF from './ComponetF'
+
+class ComponentE extends Component {
+  render() {
+    return (
+      <ComponetF />
+    )
+  }
+}
+
+export default ComponentE
