fe116aa35ff5eeb382bae342724c2f9f1f848694
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-09-01 13:27:06 +0300

    Use Counter component to render props to children component(HoverCounterTwo and ClickCounterTwo)

--- render-props/src/App.js
+++ render-props/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import logo from './logo.svg';
 import './App.css';
-import ClickCountTwo from './components/ClickCountTwo';
+import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounterTwo from './components/HoverCounterTwo';
 import User from './components/User';
 import Counter from './components/Counter';
@@ -9,10 +9,29 @@ import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <ClickCountTwo />
+      {/* <ClickCountTwo />
       <HoverCounterTwo />
-      <User render={(isLoggedIn) => isLoggedIn ? 'Igor' : 'Guest'} />
-      {/* <Counter render={(count, incrementCount) => <ClickCountTwo} /> */}
+      <User render={(isLoggedIn) => isLoggedIn ? 'Igor' : 'Guest'} /> */}
+      <Counter
+        render={
+          (count, incrementCount) => (
+            <ClickCounterTwo
+              count={count}
+              incrementCount={incrementCount}
+            />
+          )
+        }
+      />
+      <Counter
+        render={
+          (count, incrementCount) => (
+            <HoverCounterTwo
+              count={count}
+              incrementCount={incrementCount}
+            />
+          )
+        }
+      />
     </div>
   );
 }
