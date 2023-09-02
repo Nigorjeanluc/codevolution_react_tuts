52947cbc746d4047701d289b138fd40c7b769312
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-09-02 23:04:47 +0300

    GET request with axios

---
+++ react-http-request/src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import logo from './logo.svg';
+import './App.css';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
